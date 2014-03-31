909ce26b3f7f299f41173121d7696743568d9dee
Brian Gilham <me@briangilham.com>
2014-03-31 14:35:57 -0400

    Added README to project. Integrated Cocopods and installed some test pods.

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '7.0'
+inhibit_all_warnings!
+
+target :SecretSquirrelTests, exclusive: true do
+  pod 'Expecta', '~> 0.3.0'
+  pod 'OCMock', '~> 2.2.3'
+  pod 'FBSnapshotTestCase', '~> 1.1'
+end
---
+++ Pods/Pods-SecretSquirrelTests-environment.h
@@ -0,0 +1,26 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Expecta
+#define COCOAPODS_POD_AVAILABLE_Expecta
+#define COCOAPODS_VERSION_MAJOR_Expecta 0
+#define COCOAPODS_VERSION_MINOR_Expecta 3
+#define COCOAPODS_VERSION_PATCH_Expecta 0
+
+// FBSnapshotTestCase
+#define COCOAPODS_POD_AVAILABLE_FBSnapshotTestCase
+#define COCOAPODS_VERSION_MAJOR_FBSnapshotTestCase 1
+#define COCOAPODS_VERSION_MINOR_FBSnapshotTestCase 1
+#define COCOAPODS_VERSION_PATCH_FBSnapshotTestCase 0
+
+// OCMock
+#define COCOAPODS_POD_AVAILABLE_OCMock
+#define COCOAPODS_VERSION_MAJOR_OCMock 2
+#define COCOAPODS_VERSION_MINOR_OCMock 2
+#define COCOAPODS_VERSION_PATCH_OCMock 3
+
