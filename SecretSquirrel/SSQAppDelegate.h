2549489417fc4521f47f10be71f298307541d962
Brian Gilham <me@briangilham.com>
2014-03-31 14:31:50 -0400

    New initial project

---
+++ SecretSquirrel/SSQAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SSQAppDelegate.h
+//  SecretSquirrel
+//
+//  Created by Brian Gilham on 3/31/2014.
+//  Copyright (c) 2014 The Working Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSQAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
