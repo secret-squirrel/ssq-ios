ba4d941829f05acd0e69f302f16771ba37f5ac86
Brian Gilham <me@briangilham.com>
2014-03-31 14:28:17 -0400

    Initial project creation.

---
+++ Secret Squirrel/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Secret Squirrel
+//
+//  Created by Brian Gilham on 3/31/2014.
+//  Copyright (c) 2014 The Working Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SSQAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SSQAppDelegate class]));
+    }
+}
